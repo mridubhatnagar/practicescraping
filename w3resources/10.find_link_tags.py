e02eedb76c75c62f3f4fdfb3e373159867e96773
mridubhatnagar <mridubhatnagar@rediffmail.com>
2020-11-03 17:22:15 +0530

    solution to problems 14-15 from w3 resources BeautifulSoup section

---
+++ w3resources/10.find_link_tags.py
@@ -0,0 +1,9 @@
+# Q10. Write a Python program to find all the link tags and list the first ten from the webpage python.org.
+
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get("https://www.python.org").text
+soup = BeautifulSoup(page, 'lxml')
+anchor_tags = soup.find_all('a')
+print(anchor_tags[0:10])
